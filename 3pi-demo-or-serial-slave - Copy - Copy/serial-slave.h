614e0baa4f833d98c3833e8f6925f8aebe5d83bf
sunnyogosanyi <sunnyogosanyi@yahoo.com>
2018-04-05 19:47:47 +0300

    Code cleanup

--- 3pi-demo-or-serial-slave - Copy - Copy/serial-slave.h
+++
@@ -1,9 +0,0 @@
-void serial_initialize();
-void check_for_serial_slave();
-
-// Local Variables: **
-// mode: C **
-// c-basic-offset: 4 **
-// tab-width: 4 **
-// indent-tabs-mode: t **
-// end: **
